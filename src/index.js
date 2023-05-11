4dc603fa0f6639f54a8c696fde4f3f45d196eb8a
Le Cong Tuan Anh <lecongdev123@gmail.com>
2023-05-12 00:25:19 +0700

    handlebars

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { engine } = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'resources/views'));
+// app.set("views", "./views");
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+
+app.get('/new', (req, res) => {
+    res.render('new');
+});
+
+app.listen(3000);
